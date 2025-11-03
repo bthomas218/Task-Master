05cd457608afc739dc885efe95b2f4414530157c
bthomas218 <bradythomas690@gmail.com>
2025-11-03 14:13:15 -0600

    feat added input validation

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
-import express, { json } from "express";
+import express from "express";
+import Joi from "joi";
 import Task from "./task.js";
 import TaskList from "./tasklist.js";
 
@@ -11,6 +12,24 @@ tl.addTask(new Task(1, "Add POST method"));
 tl.addTask(new Task(2, "Add PATCH method"));
 tl.addTask(new Task(3, "Add DELETE method"));
 
+const taskSchema = Joi.object({
+  desc: Joi.string().required(),
+  status: Joi.string().valid("To do", "In Progress", "Complete"),
+});
+
+const taskUpdateSchema = Joi.object({
+  desc: Joi.string(),
+  status: Joi.string().valid("To do", "In Progress", "Complete"),
+});
+
+const taskListSchema = Joi.object({
+  status: Joi.string().valid("All", "To do", "In Progress", "Complete"),
+});
+
+const validate = (schema, input) => {
+  return schema.validate(input);
+};
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
@@ -18,6 +37,11 @@ app.get("/", (req, res) => {
 });
 
 app.get("/api/tasks", (req, res) => {
+  const { error } = validate(taskListSchema, req.query);
+  if (error) {
+    res.status(400).send(error.details[0].message);
+    return;
+  }
   const { status } = req.query;
   res.json(tl.listTasks(status));
 });
@@ -30,20 +54,24 @@ app.get("/api/tasks/:id", (req, res) => {
 });
 
 app.post("/api/tasks", (req, res) => {
-  try {
-    const { desc } = req.body;
-    const t = new Task(tl.len() + 1, desc);
-    tl.addTask(t);
-    res.json(t.toJSON());
-  } catch (error) {
-    res
-      .status(400)
-      .json({ "Error Name:": error.name, "Error:": error.message });
+  const { error } = validate(taskSchema, req.body);
+  if (error) {
+    res.status(400).send(error.details[0].message);
+    return;
   }
+  const { desc, status } = req.body;
+  const t = new Task(tl.len() + 1, desc, status);
+  tl.addTask(t);
+  res.json(t.toJSON());
 });
 
 app.patch("/api/tasks/:id", (req, res) => {
   const { id } = req.params;
+  const { error } = validate(taskUpdateSchema, req.body);
+  if (error) {
+    res.status(400).send(error.details[0].message);
+    return;
+  }
   const { desc, status } = req.body;
   const t = tl.getTask(parseInt(id));
   if (t) {
