652810d4c8c3f2a015e6d065250847057a8886d7
bthomas218 <bradythomas690@gmail.com>
2025-11-02 20:40:42 -0600

    feat: added get endpoints

--- server/index.js
+++ server/index.js
@@ -1,12 +1,35 @@
-import express from "express";
+import express, { json } from "express";
+import Task from "./task.js";
+import TaskList from "./tasklist.js";
 
 const PORT = process.env.port || 3000;
 const app = express();
+
+// For testing
+const tl = new TaskList();
+tl.addTask(new Task(1, "Add POST method"));
+tl.addTask(new Task(2, "Add PATCH method"));
+tl.addTask(new Task(3, "Add DELETE method"));
 
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("ONLINE");
+});
+
+app.get("/api/tasks", (req, res) => {
+  const { status } = req.query;
+  console.log(status);
+  res.json(tl.listTasks(status));
+});
+
+app.get("/api/tasks/:id", (req, res) => {
+  const { id } = req.params;
+  console.log(id);
+  const t = tl.getTask(parseInt(id));
+  console.log(t);
+  if (t) res.json(t.toJSON());
+  else res.status(404).json({ error: "task not found" });
 });
 
 app.listen(PORT, () => {
--- server/tasklist.js
+++ server/tasklist.js
@@ -1,64 +1,70 @@
-import fs from 'fs';
-import Task from './task.js';
+import fs from "fs";
+import Task from "./task.js";
+import { json } from "stream/consumers";
 
 /**
  * A class to represent a list of tasks
  * @tasks: the tasks stored on the list
  */
 export default class TaskList {
+  //  Private Field
+  #tasks;
 
-    //  Private Field
-    #tasks
+  //  Public Field
+  constructor() {
+    this.#tasks = [];
+  }
 
-    //  Public Field
-    constructor() {
-        this.#tasks = [];
-    }
+  //  Add a task
+  addTask(task) {
+    this.#tasks.push(task);
+  }
 
-    //  Add a task
-    addTask(task) {
-        this.#tasks.push(task);
-    }
+  // Update a task
+  updateTask(id, desc = null, status = null) {
+    const task = this.#tasks.find((task) => task.id === id);
+    if (desc !== null) task.desc = desc;
+    if (status !== null) task.status = status;
+  }
 
-    // Update a task
-    updateTask(id, desc = null, status = null) {
-        const task = this.#tasks.find(task => task.id === id);
-        if(desc !== null) task.desc = desc;
-        if(status !== null)  task.status = status;
-    }
+  // Delete a task
+  deleteTask(id) {
+    this.#tasks = this.#tasks.filter((t) => t.id !== id);
+  }
 
-    // Delete a task
-    deleteTask(id) {
-        this.#tasks = this.#tasks.filter(t => t.id !== id);
-    }
+  // List all tasks or list all tasks with a certain status
+  listTasks(status = "All") {
+    if (status === "All") return this.#tasks.map((t) => t.toJSON());
+    return this.#tasks.filter((task) => task.status === status);
+  }
 
-    // List all tasks or list all tasks with a certain status
-    listTasks(status = 'All')
-    {
-        if(status === 'All') return this.#tasks;
-        return this.#tasks.filter(task => task.status === status);
-    }
+  getTask(id) {
+    return this.#tasks.find((task) => task.id === id);
+  }
 
-    static loadFromFile(fileName) {
-        if(!fs.existsSync(fileName)) return new TaskList();
+  static loadFromFile(fileName) {
+    if (!fs.existsSync(fileName)) return new TaskList();
 
-        const raw = fs.readFileSync(fileName, 'utf8');
-        const data = JSON.parse(raw);
-        const taskList = new TaskList();
-        data.forEach(t => {
-            taskList.addTask(new Task(t.id, t.desc, t.status, t.createdAt), t.updatedAt);
-        });
-        return taskList;
-    }
+    const raw = fs.readFileSync(fileName, "utf8");
+    const data = JSON.parse(raw);
+    const taskList = new TaskList();
+    data.forEach((t) => {
+      taskList.addTask(
+        new Task(t.id, t.desc, t.status, t.createdAt),
+        t.updatedAt
+      );
+    });
+    return taskList;
+  }
 
-    saveToFile(fileName) {
-        const data = this.#tasks.map(t => ({
-            id: t.id,
-            desc: t.desc,
-            status: t.status,
-            createdAt: t.createdAt,
-            updatedAt: t.updatedAt,
-        }))
-        fs.writeFileSync(fileName, JSON.stringify(data, null, 2));
-    }
+  saveToFile(fileName) {
+    const data = this.#tasks.map((t) => ({
+      id: t.id,
+      desc: t.desc,
+      status: t.status,
+      createdAt: t.createdAt,
+      updatedAt: t.updatedAt,
+    }));
+    fs.writeFileSync(fileName, JSON.stringify(data, null, 2));
+  }
 }
