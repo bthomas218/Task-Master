b49b06af981038e2926a7508ab26a0168958b56e
bthomas218 <bradythomas690@gmail.com>
2025-12-07 20:19:06 -0600

    refactor: utils/errorHandler.js -> utils/errors.ts

---
+++ src/utils/errors.ts
@@ -0,0 +1,40 @@
+/**
+ * Custom Error class for handling API errors.
+ * @module utils/errorHandler
+ * @exports APIError
+ */
+export class APIError extends Error {
+  statusCode: number;
+  constructor(message: string, statusCode: number) {
+    super(message);
+    this.statusCode = statusCode;
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+/**
+ * Custom Error class for handling validation errors
+ */
+export class ValidationError extends APIError {
+  constructor(message: string) {
+    super(message, 400);
+  }
+}
+
+/**
+ * Custom Error class for handling not found errors
+ */
+export class NotFoundError extends APIError {
+  constructor(message: string) {
+    super(message, 404);
+  }
+}
+
+/**
+ * Custom Error class for handling unauthorized errors
+ */
+export class UnauthorizedError extends APIError {
+  constructor(message: string) {
+    super(message, 401);
+  }
+}
